cf9173cb68c99f1a9b0862ca388ecb3985f2c7af
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-11-28 08:36:41 +0100

    spaces

---
+++ es158/es.sh
@@ -0,0 +1,10 @@
+echo "Realizzare uno script asterischi.sh che conta quante sono le righe che \
+contengono almeno un asterisco * in tutti i file il cui nome termina con .h nella \
+parte di filesystem a partire dalla directory /usr/include/
+
+Lo script deve stampare sul proprio standard error il numero delle righe contate."
+set -v
+
+
+
+set +v
