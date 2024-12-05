180d6d66827c2e2443adfdc748b4a7f659efc92b
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-12-05 11:58:22 +0100

    correct seleziona.sh

--- lab05/es38-39/seleziona.sh
+++ lab05/es38-39/seleziona.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-TOTAL_CHAR=0
-while read ROW; do
-	if [ `echo ${ROW} | grep -c "A"` -eq 1 ]; then
-		echo "Row: ${ROW}"
-		ROW_CHAR=`echo ${ROW} | wc -m`
-		TOTAL_CHAR=$((${TOTAL_CHAR}+${ROW_CHAR}))
-	fi
-done
-echo "Total chars: ${TOTAL_CHAR}"
+# WRONG
+# TOTAL_CHAR=0
+# while read ROW; do
+# 	if [ `echo ${ROW} | grep -c "A"` -eq 1 ]; then
+# 		echo "Row: ${ROW}"
+# 		ROW_CHAR=`echo ${ROW} | wc -m`
+# 		TOTAL_CHAR=$((${TOTAL_CHAR}+${ROW_CHAR}))
+# 	fi
+# done
+# echo "Total chars: ${TOTAL_CHAR}"
+
+grep A | wc -m
