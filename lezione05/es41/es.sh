3a1ee5bc739f34c384f7437c311551a607d46517
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-12-07 16:34:13 +0100

    better naming

--- lezione05/es41/es.sh
+++ lezione05/es41/es.sh
@@ -10,3 +10,16 @@ chmod +x ./reversebizzarro.sh
 ./reversebizzarro.sh "ciao come va"
 
 set +v
+echo "41++ per tutti i file contenuti nella directory /usr/include/ (NON \
+NELLE SOTTODIRECTORY) far vedere a video le righe che contengono almeno un \
+carattere * e contare quante sono queste righe"
+set -v
+
+COUNT=0
+for f in `find /usr/include -type f -maxdepth 1`; do
+	grep "*" ${f}
+	COUNT=$((${COUNT}+`grep -c "*" ${f}`))
+done
+echo "Total: ${COUNT}" 
+
+set +v
