f9f8bbecda434aaeb1a03466a69f6789e4efedca
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-12-14 17:39:10 +0100

    better division in lines

--- lezione09/es154/voti.sh
+++ lezione09/es154/voti.sh
@@ -4,8 +4,9 @@
 
 # I voti della seconda prova li devo mettere tutti nello standard output
 
+cat esame2.txt
 
 # Poi devo mettere nello standard output i voti della prima prova
 # ottenuti dagli studenti che non hanno un voto nella seconda prova
 
-(cat esame2.txt; cut -d " " -f 1 esame2.txt | grep -v -f - esame1.txt)
+cut -d " " -f 1 esame2.txt | grep -v -f - esame1.txt
